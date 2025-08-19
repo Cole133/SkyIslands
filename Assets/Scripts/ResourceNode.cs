60d0905f4c686c5ed62a580ca0cb891a1784b857
Cole133 <colegardiner33@gmail.com>
2025-08-19 01:41:34 -0400

    Added new drops

--- Assets/Scripts/ResourceNode.cs
+++ Assets/Scripts/ResourceNode.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class ResourceNode : MonoBehaviour
 {
     public Item dropItem;
+    public Item dropItem2;
     public int dropAmount = 1;
 
     public void Harvest(Inventory inventory)
     {
         inventory.AddItem(dropItem, dropAmount);
+        inventory.AddItem(dropItem2, dropAmount);
         Destroy(gameObject); // remove the resource after collecting
     }
 }
